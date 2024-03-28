917bf5ad83722008a9384bb56b65a02a47fc8915
Abdallah-Abdelaal <abdallahabdelaal2005@gmail.com>
2024-03-28 10:46:07 -0400

    Minor changes to gui2

--- gui_file2.py
+++ gui_file2.py
@@ -98,7 +98,7 @@ class PrioritizeApp:
 
 class DropdownApp:
 
-    selected_song: Any = "Oops!...I Did It Again"
+    selected_song: Any
 
     def __init__(self, root):
         self.root = root
@@ -108,13 +108,16 @@ class DropdownApp:
 
         self.options = song_name_list
         self.value_inside = tk.StringVar(root)
-        self.value_inside.set(self.options[0])
+        self.value_inside.set("---ANY---")
 
         self.dropdown_menu = tk.OptionMenu(root, self.value_inside, *self.options)
         self.dropdown_menu.pack()
 
     def on_select(self):
         self.selected_song = self.value_inside.get()
+
+        if self.selected_song == '---ANY---':
+            raise ValueError
 
 def save_all_information(priority_list: PrioritizeApp, drag_drop_object: DropdownApp, explicit: bool):
     """
